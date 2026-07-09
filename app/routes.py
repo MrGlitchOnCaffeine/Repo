abc29c4b738efdd2416415b1ca75af98f57ce1a2
MrGlitchOnCaffeine <Mrglitchoncaffeine@gmail.com>
2026-07-09 16:45:08 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,37 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    if current_user.is_authenticated and current_user.is_admin():
+        return redirect(url_for('main.admin_dashboard'))
+
+    home_data = {
+        'is_authenticated': False,
+        'user_name': None,
+        'latest_application': None,
+        'has_applications': False,
+    }
+
+    if current_user.is_authenticated:
+        applications = (
+            LoanApplication.query
+            .filter_by(user_id=current_user.id)
+            .order_by(LoanApplication.application_date.desc())
+            .all()
+        )
+
+        latest_application = applications[0] if applications else None
+
+        home_data = {
+            'is_authenticated': True,
+            'user_name': current_user.full_name or current_user.email,
+            'latest_application': latest_application,
+            'has_applications': len(applications) > 0,
+        }
+
+    return render_template(
+        'index.html',
+        home_data=home_data
+    )
 
 
 @main.route('/register', methods=['GET', 'POST'])
